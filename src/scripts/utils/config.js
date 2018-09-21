8e0e10e4259d015126d7b8cd35d20c488c614539
Ajeesh Rajan <arajan4473@altimetrik.com>
2018-09-21 12:09:24 +0530

    port update

--- src/scripts/utils/config.js
+++ src/scripts/utils/config.js
@@ -1,4 +1,4 @@
 export default {
-    baseUrl: process.env.NODE_ENV === 'production' ? 'http://207.148.74.152/7001' : 'http://207.148.74.152/7001',
+    baseUrl: process.env.NODE_ENV === 'production' ? 'http://207.148.74.152:7001' : 'http://207.148.74.152:7001',
     version : ''
 }
